58fd917559c2c83177454a5f0d95eaacf7425b10
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-04-03 15:23:33 -0300

    System tray with clock and draggable recycle bin Array of shortcuts under Destop to further implementation with useReducer

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+
+const Shortcut = (props) =>{
+  return(
+    <g>
+      <image 
+        x={props.position.x}
+        y={props.position.y}
+        width='50'
+        height='50'
+        href={props.source}
+      />
+      <text
+        className='shortcut-text'
+        x={props.position.x + props.textAlign}
+        y={props.position.y + 70}
+      >
+        {props.text}
+      </text>
+
+    </g>
+  );
+}
+
+
+const Tray = () =>{
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(()=>{
+    const id = setInterval(() => {
+      setTime(() => new Date())}, 60000);
+    return () => {clearInterval(id)};
+  }, []);
+
+  return(
+    <div 
+      id='tray'
+      style={{width: '150px'}}
+    >
+      <div id='clock'>
+        {time.toLocaleString([], {hour: '2-digit', minute: '2-digit'})}
+      </div>
+    </div>
+  )
+}
 
 const StartButton = () =>{
   return(
@@ -13,6 +58,7 @@ const TaskBar = () => {
   return(
     <div id='taskBar'>
       <StartButton />
+      <Tray />
     </div>
   );
 };
@@ -21,6 +67,7 @@ const Selection = (props) =>{
   const wid = props.mousePosition.x - props.initialPos.x;
   const hei = props.mousePosition.y - props.initialPos.y;
   let px, py, rot;
+  px = py = rot = 0;
   if(wid < 0 && hei < 0){
     rot = 180;
     px = props.initialPos.x;
@@ -37,7 +84,7 @@ const Selection = (props) =>{
   return(
     <rect 
       style={{
-        display: props.visible,
+        display: props.visible ? 'block': 'none',
       }}
       x={props.initialPos.x}
       y= {props.initialPos.y}
@@ -54,16 +101,27 @@ const Selection = (props) =>{
 const Desktop = () => {
   const [initialPos, handleInitialPos] = useState({x:0, y:0})
   const [mouseMove, handleMouseMove] = useState({x:0, y:0});
-  const [showSelect, handleShowSelect] = useState('none')
+  const [showSelect, handleShowSelect] = useState(false)
+  const [shortcutPos, handleShortcutPos] = useState([{x:50, y:50}])
+  const [shortcutMove, handleShortcutMove] = useState(-1)
 
   const mouseClicked = event =>{
-    handleMouseMove({x:event.clientX, y: event.clientY});
-    handleInitialPos({x:event.clientX, y: event.clientY});
-    handleShowSelect('block')
+    handleShortcutMove(shortcutPos.findIndex(i =>(
+      event.clientX >= i.x && event.clientX <= i.x + 50 &&
+      event.clientY >= i.y && event.clientY <= i.y + 50
+    )))
+
+    if(shortcutMove === -1){
+      handleMouseMove({x:event.clientX, y: event.clientY});
+      handleInitialPos({x:event.clientX, y: event.clientY});
+      handleShowSelect(true)
+    }
   }
 
   const mouseMoved = event =>{
-    handleMouseMove({x:event.clientX, y: event.clientY})
+    shortcutMove !== -1 ? 
+      handleShortcutPos([{x: event.clientX, y: event.clientY}]):
+      handleMouseMove({x:event.clientX, y: event.clientY})
   }
   return(
     <div id='desktopContainer'>
@@ -71,12 +129,21 @@ const Desktop = () => {
         id='desktop'
         onMouseDown={(e)=>{mouseClicked(e)}}
         onMouseMove={(e)=>{mouseMoved(e)}}
-        onMouseUp={()=>{handleShowSelect('none')}}
+        onMouseUp={()=>{
+          handleShowSelect(false); 
+          handleShortcutMove(-1);
+        }}
       >
         <Selection 
           initialPos={initialPos}
           mousePosition={mouseMove}
           visible={showSelect}
+        />
+        <Shortcut 
+          source= '/empty-recycle.png'
+          text= 'Recycle Bin'
+          textAlign= {-15}
+          position={shortcutPos[0]}
         />
       </svg>
     </div>
